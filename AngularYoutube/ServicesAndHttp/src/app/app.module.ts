d4119935e2f7562be6ad5b88f3e6e6cfa9a769b9
vincent tene <vincent.tene@gmail.com>
2020-09-21 16:08:50 +0200

    to Chapter 22

---
+++ AngularYoutube/ServicesAndHttp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AngularYoutube/ServicesAndHttp/src/app/employee.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+ 
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+
+ 
+import { IEmployee } from './employee';
+import { Observable } from 'rxjs/internal/Observable';
+import { throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private _url: string = "/assets/data/employees.json";
+
+
+  constructor(private http:HttpClient) { }
+
+  getEmployees(): Observable<IEmployee[]>{
+    return this.http.get<IEmployee[]>(this._url).pipe(
+      catchError(this.errorHandler) );
+  }
+
+  
+  errorHandler(error: HttpErrorResponse){
+    return Observable.throw(error.message || "Server Error");
+  }
+
+
+}
